78936836e29ad34c7ffd092867fb2f111c951a4b
Amit P Naik <amit.naik8103@gmail.com>
2020-08-09 17:30:07 +0530

    initail app structure for react

---
+++ redux-boilerplatecode/src/components/about/AboutPage.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class AboutPage extends Component {
+    render() {
+        return (
+            <div>
+                <h2>About</h2>
+                <p>
+                    This app uses React, Redux, React Router, and many other helpful
+                    libraries.
+                </p>
+            </div>
+        )
+    }
+}
+
+export default AboutPage;
